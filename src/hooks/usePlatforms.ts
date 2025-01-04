8ba07012b6915b192c0d51df4c4adac2fdefcb71
Denakpo Agba <agbagerard@gmail.com>
2025-01-04 16:02:51 -0500

    fetching the platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import { FetchResponse } from "./useData";
+import apiClients from "../services/api-clients";
+
 
 interface Platform { 
     id: number;
@@ -6,6 +9,12 @@ interface Platform {
     slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
-
+const usePlatforms = () => useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+        apiClients
+            .get<FetchResponse<Platform>>('/platforms/lists/parents')
+            .then(res => res.data),
+    staleTime: 1000 * 60 * 60 * 24, // 24 hours
+});
 export default usePlatforms;
