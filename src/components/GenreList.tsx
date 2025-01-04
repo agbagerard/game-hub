c77a3d4e37c8d17cb5b5d312ac4095f36f6870e7
Denakpo Agba <agbagerard@gmail.com>
2025-01-04 18:03:51 -0500

    refactor-simplify game query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -13,10 +13,13 @@ import { h1 } from "framer-motion/client";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
 
-const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
+const GenreList = ({
+  selectedGenreId,
+  onSelectGenre,
+}: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -24,7 +27,9 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 
   return (
     <>
-      <Heading as={h1} fontSize={'2xl'} marginBottom={'3'}>Genres</Heading>
+      <Heading as={h1} fontSize={"2xl"} marginBottom={"3"}>
+        Genres
+      </Heading>
       <List>
         {data?.results.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
@@ -38,7 +43,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
               <Button
                 whiteSpace={"normal"}
                 textAlign={"left"}
-                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                fontWeight={genre.id === selectedGenreId ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
                 fontSize="lg"
                 variant="link"
