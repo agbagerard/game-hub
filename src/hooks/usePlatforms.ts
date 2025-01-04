013fe108441cf7830d780fd208182972893cc33a
Denakpo Agba <agbagerard@gmail.com>
2025-01-04 16:30:11 -0500

    refactoring - remove duplicate interface

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -3,7 +3,7 @@ import { FetchResponse } from "../services/api-clients";
 import apiClients from "../services/api-clients";
 
 
-interface Platform { 
+export interface Platform { 
     id: number;
     name: string;
     slug: string;
