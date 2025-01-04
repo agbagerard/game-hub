072d8069609a8d2f2e43362180b0cdda6ea5a349
Denakpo Agba <agbagerard@gmail.com>
2025-01-04 18:28:49 -0500

    refactor-create look up hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
 import APIClient from '../services/api-client';
-import genres from '../data/genres';
 
 const apiClient = new APIClient<Genre>('/genres');
 
@@ -14,7 +13,7 @@ const useGenres = () => useQuery({
     queryKey: ['genres'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
-    initialData: {count: genres.length, results: genres},
+    // initialData: genres,
 })
 
 export default useGenres;
