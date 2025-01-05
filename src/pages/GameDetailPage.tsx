753ce4d068f13451e7ddd699a5d8ddc8d78a0ee0
Denakpo Agba <agbagerard@gmail.com>
2025-01-05 15:37:19 -0500

    Build games attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
-
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -11,13 +11,14 @@ const GameDetailPage = () => {
   if (isLoading) return <Spinner />;
 
   if (error || !game) throw error;
-  
+
   return (
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
-  )
-}
+  );
+};
 
-export default GameDetailPage
+export default GameDetailPage;
